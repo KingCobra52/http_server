7a97bb32d561b5e25bac3806357e6cbcc555885b
Siddarth Thota <hellotheking52@gmail.com>
2026-07-10 13:06:06 -0400

    first http server implementation - single-socket without great error handling

--- experiments/sockets/first_while.py
+++ experiments/sockets/first_while.py
@@ -6,21 +6,37 @@ socket.listen(5)
 print('Server is listening on http://127.0.0.1:8080 ...')
 
 while True:
+
+    try:
     #client_socket is the socket we created to talk to the
-    client_socket, client_address = socket.accept()
+        client_socket, client_address = socket.accept()
 
-    raw_request = client_socket.recv(1024)
-    request_text = raw_request.decode('utf-8')
+        raw_request = client_socket.recv(1024)
+        request_text = raw_request.decode('utf-8')
 
-    print(f'Recieved request from {client_address}:\n{request_text}')
+        print(f"Received request from {client_address}:\n{request_text}\n")
 
-    #example valid http response
-    response_text = (
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/html\r\n"
-            "\r\n"
-            "<html><body><h1>Hello from your custom server!</h1></body></html>"
-        )
+        path = request_text.split(" ")[1]
 
-    client_socket.send(response_text.encode('utf-8'))
-    client_socket.close()
+        if path == "/":
+            status_line = "HTTP/1.1 200 OK"
+            response_body = "<html><body><h1>Hello from your custom server!</h1></body></html>"
+        elif path == "/about":
+            status_line = "HTTP/1.1 200 OK"
+            response_body = "<html><body><h1>About Us</h1><p>We built this from scratch!</p></body></html>"
+        else:
+            status_line = "HTTP/1.1 404 NOT FOUND"
+            response_body = "<html><body><h1>404 NOT FOUND</h1></body></html>"
+
+        response_text = (
+                f"{status_line}\r\n"
+                "Content-Type: text/html\r\n"
+                "\r\n"
+                f"{response_body}"
+            )
+
+        client_socket.send(response_text.encode('utf-8'))
+        client_socket.close()
+
+    except Exception as e:
+        print(f"Error occured while handling a request: {e}")
